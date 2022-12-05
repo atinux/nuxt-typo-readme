7e402bde6020adac35b415f4147085f2c261b79e
Sébastien Chopin <seb@nuxtjs.com>
2022-12-05 13:25:59 +0100

    chore: disable ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
   extends: [
     process.env.TYPOGRAPHY_THEME || '@nuxt-themes/typography'
   ],
@@ -8,9 +8,9 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@vueuse/nuxt'
   ],
-  content: {
-    experimental: {
-      clientDB: true
-    }
-  }
+  // content: {
+  //   experimental: {
+  //     clientDB: true
+  //   }
+  // }
 })
