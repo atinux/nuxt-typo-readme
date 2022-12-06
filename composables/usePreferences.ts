6c36fd122213a88810adaa66c02dafce919c8485
Sébastien Chopin <seb@nuxtjs.com>
2022-12-06 10:54:50 +0100

    Delete usePreferences.ts

--- composables/usePreferences.ts
+++
@@ -1,9 +0,0 @@
-import type { BUNDLED_THEMES } from 'shiki-es'
-
-export function usePreferences() {
-  const syntaxHighlightTheme = useLocalStorage('syntaxHighlightTheme', 'github-dark' as typeof BUNDLED_THEMES[0])
-
-  return {
-    syntaxHighlightTheme
-  }
-}
